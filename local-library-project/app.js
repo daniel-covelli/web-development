0ea3f7f7f399ca63dfc5856184c97a15abf48837
Daniel Covelli <covellidanny@gmail.com>
2020-06-20 19:01:42 -0700

    Final Commit

--- local-library-project/app.js
+++ local-library-project/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+
 var compression = require('compression');
 var helmet = require('helmet');
 
